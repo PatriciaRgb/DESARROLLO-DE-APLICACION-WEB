70888390bf8eaa3ccbbba68d59a7d432f5f43fd1
Patricia Segura Reséndiz <patyresendiz622@gmail.com>
2026-08-26 20:43:48 -0600

    Crear primera pagina web con PHP

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Práctica 01 - PHP</title>
+</head>
+<body>
+
+    <h1>Mi primera página web con PHP</h1>
+
+    <?php
+        echo "<p>Hola, esta página está ejecutando PHP.</p>";
+    ?>
+
+</body>
+</html>
